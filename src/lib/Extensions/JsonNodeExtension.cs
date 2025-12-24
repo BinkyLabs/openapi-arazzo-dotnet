52edc4024bd6b9102b79a89755dc1fe5c1c6ba3a
Vincent Biret <vincentbiret@hotmail.com>
2025-12-23 21:20:48 -0500

    chore: additional serialization infrastructure
    Signed-off-by: Vincent Biret <vincentbiret@hotmail.com>

---
+++ src/lib/Extensions/JsonNodeExtension.cs
@@ -0,0 +1,36 @@
+﻿
+// Licensed under the MIT license.
+
+using System.Text.Json.Nodes;
+
+using Microsoft.OpenApi;
+
+namespace BinkyLabs.OpenApi.Arazzo;
+
+/// <summary>
+/// A wrapper class for JsonNode
+/// </summary>
+public class JsonNodeExtension : IOpenApiElement, IArazzoExtension
+{
+    private readonly JsonNode jsonNode;
+
+    /// <summary>
+    /// Initializes the <see cref="JsonNodeExtension"/> class.
+    /// </summary>
+    /// <param name="jsonNode"></param>
+    public JsonNodeExtension(JsonNode jsonNode)
+    {
+        this.jsonNode = jsonNode;
+    }
+
+    /// <summary>
+    /// Gets the underlying JsonNode.
+    /// </summary>
+    public JsonNode Node { get { return jsonNode; } }
+
+    /// <inheritdoc/>
+    public void Write(IOpenApiWriter writer, ArazzoSpecVersion specVersion)
+    {
+        writer.WriteAny(Node);
+    }
+}
