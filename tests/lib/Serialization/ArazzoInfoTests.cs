f47629e457ef06bf70071df02389c899b7fa9374
Vincent Biret <vincentbiret@hotmail.com>
2025-12-29 10:28:17 -0500

    tests: adds unit tests for info object
    Signed-off-by: Vincent Biret <vincentbiret@hotmail.com>

---
+++ tests/lib/Serialization/ArazzoInfoTests.cs
@@ -0,0 +1,67 @@
+using System.Text.Json.Nodes;
+
+using BinkyLabs.OpenApi.Arazzo.Reader;
+using BinkyLabs.OpenApi.Arazzo.Reader.V1;
+
+using Microsoft.OpenApi;
+
+using ParsingContext = BinkyLabs.OpenApi.Arazzo.Reader.ParsingContext;
+
+namespace BinkyLabs.OpenApi.Arazzo.Tests;
+
+public class ArazzoInfoTests
+{
+    [Fact]
+    public void SerializeAsV1_ShouldWriteCorrectJson()
+    {
+        // Arrange
+        var overlayInfo = new ArazzoInfo
+        {
+            Title = "Test Overlay",
+            Version = "1.0.0"
+        };
+        using var textWriter = new StringWriter();
+        var writer = new OpenApiJsonWriter(textWriter);
+
+        var expectedJson =
+"""
+{
+    "title": "Test Overlay",
+    "version": "1.0.0"
+}
+""";
+
+        // Act
+        overlayInfo.SerializeAsV1(writer);
+        var jsonResult = textWriter.ToString();
+        var jsonResultObject = JsonNode.Parse(jsonResult);
+        var expectedJsonObject = JsonNode.Parse(expectedJson);
+
+
+        // Assert
+        Assert.True(JsonNode.DeepEquals(jsonResultObject, expectedJsonObject), "The serialized JSON does not match the expected JSON.");
+    }
+
+    [Fact]
+    public void Deserialize_ShouldSetPropertiesCorrectly()
+    {
+        // Arrange
+        var json = """
+        {
+            "title": "Test Overlay",
+            "version": "1.0.0"
+        }
+        """;
+        var jsonNode = JsonNode.Parse(json)!;
+        var parsingContext = new ParsingContext(new());
+        var parseNode = new MapNode(parsingContext, jsonNode);
+
+
+        // Act
+        var arazzoInfo = ArazzoV1Deserializer.LoadInfo(parseNode);
+
+        // Assert
+        Assert.Equal("Test Overlay", arazzoInfo.Title);
+        Assert.Equal("1.0.0", arazzoInfo.Version);
+    }
+}
